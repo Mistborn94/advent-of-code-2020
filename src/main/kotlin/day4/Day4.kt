1a314904d90efd057bc227822f90d850cadac844
Renette Ros <supergirl0894+git@gmail.com>
2020-12-04 10:43:46 +0200

    Simplify Day 4

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,12 +1,12 @@
 package day4
 
 fun solveA(text: String): Int {
-    val lines = text.replace("\r", "").replace("\n\n", "|").replace("\n", " ").split("|")
+    val lines = text.replace("\r", "").split("\n\n")
 
     val requiredFields = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
     return lines.count { passport ->
-        passport.split(" ")
+        passport.trim().split(" ", "\n")
             .map { item -> item.substringBefore(":") }
             .containsAll(requiredFields)
     }
@@ -14,17 +14,16 @@ fun solveA(text: String): Int {
 
 fun solveB(text: String): Int {
 
-    val lines = text.replace("\r", "").replace("\n\n", "|").replace("\n", " ").split("|")
+    val lines = text.replace("\r", "").split("\n\n")
 
-    val numerical = "\\d+".toRegex()
     val hclRegex = "#[0-9a-f]{6}".toRegex()
     val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
     val pidRegex = "\\d{9}".toRegex()
 
     val requiredFields = mapOf<String, (String) -> Boolean>(
-        "byr" to { it.matches(numerical) && it.toInt() in 1920..2002 },
-        "iyr" to { it.matches(numerical) && it.toInt() in 2010..2020 },
-        "eyr" to { it.matches(numerical) && it.toInt() in 2020..2030 },
+        "byr" to { it.toInt() in 1920..2002 },
+        "iyr" to { it.toInt() in 2010..2020 },
+        "eyr" to { it.toInt() in 2020..2030 },
         "hgt" to {
             (it.endsWith("cm") && it.substringBefore("c").toInt() in 150..193)
                     || (it.endsWith("in") && it.substringBefore("i").toInt() in 59..76)
@@ -35,7 +34,7 @@ fun solveB(text: String): Int {
     )
 
     return lines.count { passport ->
-        passport.trim().split(" ")
+        passport.trim().split(" ", "\n")
             .map { item -> item.split(":") }
             .filter { (key, value) -> requiredFields[key]?.invoke(value) ?: true }
             .map { (key, _) -> key }
