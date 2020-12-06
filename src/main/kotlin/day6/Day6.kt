d455ba4af7a2a352b75265bf3f983dbf6396be32
Renette Ros <supergirl0894+git@gmail.com>
2020-12-06 11:38:46 +0200

    Day 6 WIP

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -1,10 +1,21 @@
 package day6
 
-fun solveA(lines: List<String>): Int {
-    TODO("Not implemented")
+fun solveA(text: String): Int {
+    return text.replace("\r", "")
+        .split("\n\n")
+        .map {
+            it.toCharArray()
+                .filter { char -> char in 'a'..'z' }
+                .toSet()
+        }.sumBy { it.size }
 }
 
-
-fun solveB(lines: List<String>): Int {
-    TODO("Not implemented")
+fun solveB(text: String): Int {
+    return text.replace("\r", "")
+        .split("\n\n")
+        .map { it.lines() }
+        .map {
+            it.fold(('a'..'z').toSet()) { acc, s -> s.toSet().intersect(acc) }
+        }
+        .sumBy { it.size }
 }
--- src/test/kotlin/day6/Day6KtTest.kt
+++ src/test/kotlin/day6/Day6KtTest.kt
@@ -2,40 +2,40 @@ package day6
 
 import helper.readDayFile
 import org.junit.jupiter.api.Assertions.assertEquals
-import kotlin.test.Ignore
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
-@Ignore
 internal class Day6KtTest {
 
     val day = 6
 
     @Test
     fun sample1() {
-        val lines = readDayFile(day, "sample1.in").readLines()
+        val lines = readDayFile(day, "sample1.in").readText()
 
-        assertEquals(0, solveA(lines))
-        assertEquals(0, solveB(lines))
+        assertEquals(11, solveA(lines))
+        assertEquals(6, solveB(lines))
     }
 
     @Test
     fun sample2() {
-        val lines = readDayFile(day, "sample2.in").readLines()
+        val lines = readDayFile(day, "sample2.in").readText()
 
-        assertEquals(0, solveA(lines))
-        assertEquals(0, solveB(lines))
+        assertEquals(6630, solveA(lines))
+        assertEquals(3437, solveB(lines))
     }
 
     @Test
     fun solve() {
-        val lines = readDayFile(day, "input").readLines()
+        val lines = readDayFile(day, "input").readText()
 
         val solveA = solveA(lines)
         println("A: $solveA")
-        assertEquals(0, solveA)
+        assertEquals(6587, solveA)
 
         val solveB = solveB(lines)
         println("B: $solveB")
-        assertEquals(0, solveB)
+        assertTrue(solveB > 3211)
+//        assertEquals(0, solveB)
     }
 }
