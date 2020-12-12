5f3a1544cf24da9cbb05f5d0e5748e96fd5fd927
Renette Ros <supergirl0894+git@gmail.com>
2020-12-12 08:40:01 +0200

    Change Day 12 to use fold

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -1,81 +1,37 @@
 package day12
 
-import day12.Direction.*
+import helper.Direction.*
 import helper.Point
 
-enum class Direction(val point: Point) {
+fun solveA(lines: List<String>): Int =
+    lines.fold(Pair(EAST, Point(0, 0))) { (direction, position), line ->
+        val action = line[0]
+        val value = line.substring(1, line.length).toInt()
 
-    NORTH(Point(0, 1)) {
-        override val right: Direction
-            get() = EAST
-        override val left: Direction
-            get() = WEST
-    },
-    EAST(Point(1, 0)) {
-        override val right: Direction
-            get() = SOUTH
-        override val left: Direction
-            get() = NORTH
-    },
-    SOUTH(Point(0, -1)) {
-        override val right: Direction
-            get() = WEST
-        override val left: Direction
-            get() = EAST
-    },
-    WEST(Point(-1, 0)) {
-        override val right: Direction
-            get() = NORTH
-        override val left: Direction
-            get() = SOUTH
-    };
+        when (action) {
+            'N' -> Pair(direction, position + NORTH.point * value)
+            'S' -> Pair(direction, position + SOUTH.point * value)
+            'E' -> Pair(direction, position + EAST.point * value)
+            'W' -> Pair(direction, position + WEST.point * value)
+            'L' -> Pair(direction.left(value / 90), position)
+            'R' -> Pair(direction.right(value / 90), position)
+            'F' -> Pair(direction, position + direction.point * value)
+            else -> throw UnsupportedOperationException()
+        }
+    }.second.abs()
 
-    abstract val right: Direction
-    abstract val left: Direction
-}
-
-fun solveA(lines: List<String>): Int {
-    var currentPosition = Point(0, 0)
-    var currentDirection = EAST
-
-    lines.forEach { line ->
+fun solveB(lines: List<String>): Int =
+    lines.fold(Pair(Point(0, 0), Point(10, 1))) { (ship, waypoint), line ->
         val action = line[0]
         val value = line.substring(1, line.length).toInt()
         when (action) {
-            'N' -> currentPosition += NORTH.point * value
-            'S' -> currentPosition += SOUTH.point * value
-            'E' -> currentPosition += EAST.point * value
-            'W' -> currentPosition += WEST.point * value
-            'L' -> repeat(value / 90) {
-                currentDirection = currentDirection.left
-            }
-            'R' -> repeat(value / 90) {
-                currentDirection = currentDirection.right
-            }
-            'F' -> currentPosition += currentDirection.point * value
+            'N' -> Pair(ship, waypoint + NORTH.point * value)
+            'S' -> Pair(ship, waypoint + SOUTH.point * value)
+            'E' -> Pair(ship, waypoint + EAST.point * value)
+            'W' -> Pair(ship, waypoint + WEST.point * value)
+            'L' -> Pair(ship, waypoint.counterClockwise(value))
+            'R' -> Pair(ship, waypoint.clockwise(value))
+            'F' -> Pair(ship + waypoint * value, waypoint)
+            else -> throw UnsupportedOperationException()
         }
-
-    }
-    return currentPosition.abs()
-}
-
-fun solveB(lines: List<String>): Int {
-    var waypointPosition = Point(10, 1)
-    var shipPosition = Point(0, 0)
-
-    lines.forEach { line ->
-        val action = line[0]
-        val value = line.substring(1, line.length).toInt()
-        when (action) {
-            'N' -> waypointPosition += NORTH.point * value
-            'S' -> waypointPosition += SOUTH.point * value
-            'E' -> waypointPosition += EAST.point * value
-            'W' -> waypointPosition += WEST.point * value
-            'L' -> waypointPosition = waypointPosition.counterClockwise(value)
-            'R' -> waypointPosition = waypointPosition.clockwise(value)
-            'F' -> shipPosition += waypointPosition * value
-        }
-
-    }
-    return shipPosition.abs()
-}
+    }.first.abs()
