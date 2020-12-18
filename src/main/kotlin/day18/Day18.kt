80ee7d137922c1162b7bbf688697dd3f9a200b0c
Renette Ros <supergirl0894+git@gmail.com>
2020-12-18 08:57:40 +0200

    Use the shunting yard algorithm to solve Day 18

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -3,120 +3,64 @@ package day18
 import java.util.*
 
 fun solveA(lines: List<String>): Long {
-
-    return lines.map { evaluateA(it.toCharArray().filter { char -> char != ' ' }) }.sum()
-}
-
-fun evaluateA(parts: List<Char>): Long {
-    val numbers: Stack<Long> = Stack()
-
-    var index = 0
-    while (index < parts.size) {
-        val first = parts[index]
-        when {
-            first == '(' -> {
-                val (nextIndex, nextValue) = nextExpressionA(parts, index)
-                numbers.push(nextValue)
-                index = nextIndex
-            }
-            first.isDigit() -> {
-                numbers.push(first.toString().toLong())
-                index += 1
-            }
-            first == '+' -> {
-                val (nextIndex, nextValue) = nextExpressionA(parts, index + 1)
-                numbers.push(numbers.pop() + nextValue)
-                index = nextIndex
-            }
-            first == '*' -> {
-                val (nextIndex, nextValue) = nextExpressionA(parts, index + 1)
-                numbers.push(numbers.pop() * nextValue)
-                index = nextIndex
-            }
-        }
-    }
-    assert(numbers.size == 1)
-    return numbers.pop()
-}
-
-fun nextExpressionA(parts: List<Char>, startingIndex: Int): Pair<Int, Long> {
-    val first = parts[startingIndex]
-
-    when {
-        first.isDigit() -> return Pair(startingIndex + 1, first.toString().toLong())
-        first == '(' -> {
-            var bracketCount = 1
-            var lastIndex = startingIndex + 1
-            while (bracketCount != 0) {
-                when (parts[lastIndex]) {
-                    '(' -> bracketCount++
-                    ')' -> bracketCount--
-                }
-                lastIndex++
-            }
-            val subList = parts.subList(startingIndex + 1, lastIndex - 1)
-            val evaluate = evaluateA(subList)
-            return Pair(lastIndex, evaluate)
-        }
-        else -> throw IllegalArgumentException("Unexpected Character $first")
-    }
+    return lines.map { it.toCharArray().filter { char -> char != ' ' } }
+        .map { buildPostfix(it, mapOf('*' to 1, '+' to 1)) }
+        .map { evaluatePostfix(it) }
+        .sum()
 }
 
 fun solveB(lines: List<String>): Long {
-    return lines.map { evaluateB(it.toCharArray().filter { char -> char != ' ' }) }.sum()
+    return lines.map { it.toCharArray().filter { char -> char != ' ' } }
+        .map { buildPostfix(it, mapOf('*' to 1, '+' to 2)) }
+        .map { evaluatePostfix(it) }
+        .sum()
 }
 
-fun evaluateB(parts: List<Char>): Long {
-    val numbers: Stack<Long> = Stack()
+/**
+ * The magic here is called the Shunting Yard Algorithm
+ * https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+ */
+private fun buildPostfix(tokens: List<Char>, operatorPrecedence: Map<Char, Int>): Queue<Char> {
+    val output: Queue<Char> = LinkedList()
+    val operators: Stack<Char> = Stack()
 
-    var index = 0
-    while (index < parts.size) {
-        val first = parts[index]
-        when {
-            first == '(' -> {
-                val (nextIndex, nextValue) = nextExpressionB(parts, index)
-                numbers.push(nextValue)
-                index = nextIndex
-            }
-            first.isDigit() -> {
-                numbers.push(first.toString().toLong())
-                index += 1
-            }
-            first == '+' -> {
-                val (nextIndex, nextValue) = nextExpressionB(parts, index + 1)
-                numbers.push(numbers.pop() + nextValue)
-                index = nextIndex
-            }
-            first == '*' -> {
-                val nextValue = evaluateB(parts.subList(index + 1, parts.size))
-                numbers.push(numbers.pop() * nextValue)
-                index = parts.size
-            }
-        }
-    }
-    assert(numbers.size == 1)
-    return numbers.pop()
-}
-
-fun nextExpressionB(parts: List<Char>, startingIndex: Int): Pair<Int, Long> {
-    val first = parts[startingIndex]
-
-    when {
-        first.isDigit() -> return Pair(startingIndex + 1, first.toString().toLong())
-        first == '(' -> {
-            var bracketCount = 1
-            var lastIndex = startingIndex + 1
-            while (bracketCount != 0) {
-                when (parts[lastIndex]) {
-                    '(' -> bracketCount++
-                    ')' -> bracketCount--
+    tokens.forEach { token ->
+        when (token) {
+            in '0'..'9' -> output.add(token)
+            '(' -> operators.add(token)
+            '+', '*' -> {
+                while (operators.size > 0
+                    && operators.peek() != '('
+                    && operatorPrecedence[operators.peek()]!! >= operatorPrecedence[token]!!
+                ) {
+                    output.add(operators.pop())
                 }
-                lastIndex++
+                operators.push(token)
             }
-            val subList = parts.subList(startingIndex + 1, lastIndex - 1)
-            val evaluate = evaluateB(subList)
-            return Pair(lastIndex, evaluate)
+            ')' -> {
+                while (operators.peek() != '(') {
+                    output.add(operators.pop())
+                }
+                operators.pop()
+            }
         }
-        else -> throw IllegalArgumentException("Unexpected Character $first")
     }
+
+    while (!operators.isEmpty()) {
+        output.add(operators.pop())
+    }
+    return output
+}
+
+private fun evaluatePostfix(postfix: Queue<Char>): Long {
+    val numbers = Stack<Long>()
+
+    postfix.forEach { token ->
+        when (token) {
+            in '0'..'9' -> numbers.push(token.toString().toLong())
+            '+' -> numbers.push(numbers.pop() + numbers.pop())
+            '*' -> numbers.push(numbers.pop() * numbers.pop())
+        }
+    }
+    return numbers.pop()
 }
