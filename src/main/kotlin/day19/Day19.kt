e5b123e1e6d506a59b1998ee269aa5fbbb2a76f1
Renette Ros <supergirl0894+git@gmail.com>
2020-12-19 09:21:16 +0200

    Cleanup day 19

--- src/main/kotlin/day19/Day19.kt
+++ src/main/kotlin/day19/Day19.kt
@@ -2,44 +2,38 @@ package day19
 
 sealed class Rule {
 
-    private var calculatedRegex: String? = null
-
-    protected abstract fun calculateRegex(rules: List<Rule>): String
-
-    open fun matches(rules: List<Rule>, message: String): Boolean {
-        val regex = getRegex(rules).toRegex()
-        return regex.matches(message)
-    }
-
-    fun getRegex(rules: List<Rule>): String {
-        if (calculatedRegex == null) {
-            calculatedRegex = calculateRegex(rules)
-        }
-        return calculatedRegex!!
-    }
+    abstract fun toRegex(rules: List<Rule>): String
 
     class LetterRule(val letter: String) : Rule() {
-        override fun calculateRegex(rules: List<Rule>): String = letter
+        override fun toRegex(rules: List<Rule>): String = letter
     }
 
     open class SequenceRule(val numbers: List<Int>) : Rule() {
-        override fun calculateRegex(rules: List<Rule>): String =
-            numbers.joinToString(separator = "") { rules[it].getRegex(rules) }
+        override fun toRegex(rules: List<Rule>): String =
+            numbers.joinToString("") { rules[it].toRegex(rules) }
     }
 
-    class Rule0 : Rule() {
-        override fun calculateRegex(rules: List<Rule>): String {
-            val rule42Regex = rules[42].getRegex(rules)
-            val rule31Regex = rules[31].getRegex(rules)
+    class OrRule(val sequence1: List<Int>, val sequence2: List<Int>) : Rule() {
+        override fun toRegex(rules: List<Rule>): String {
+            val part1 = sequence1.joinToString("") { rules[it].toRegex(rules) }
+            val part2 = sequence2.joinToString("") { rules[it].toRegex(rules) }
+            return "(?:$part1|$part2)"
+        }
+    }
+
+    object Rule0B : Rule() {
+        override fun toRegex(rules: List<Rule>): String {
+            val rule42Regex = rules[42].toRegex(rules)
+            val rule31Regex = rules[31].toRegex(rules)
 
             return "((?:$rule42Regex)+)((?:$rule31Regex)+)"
         }
 
-        override fun matches(rules: List<Rule>, message: String): Boolean {
-            val matchResult = getRegex(rules).toRegex().matchEntire(message)
+        fun matches(rules: List<Rule>, message: String): Boolean {
+            val matchResult = toRegex(rules).toRegex().matchEntire(message)
 
-            val rule42Regex = rules[42].getRegex(rules)
-            val rule31Regex = rules[31].getRegex(rules)
+            val rule42Regex = rules[42].toRegex(rules)
+            val rule31Regex = rules[31].toRegex(rules)
 
             if (matchResult != null) {
                 val rule42Matches = rule42Regex.toRegex().findAll(matchResult.groups[1]!!.value)
@@ -50,31 +44,31 @@ sealed class Rule {
         }
 
     }
-
-    class OrRule(val sequence1: List<Int>, val sequence2: List<Int>) : Rule() {
-
-        override fun calculateRegex(rules: List<Rule>): String {
-            val part1 = sequence1.joinToString(separator = "") { rules[it].getRegex(rules) }
-            val part2 = sequence2.joinToString(separator = "") { rules[it].getRegex(rules) }
-            return "(?:$part1|$part2)"
-        }
-    }
-
 }
 
 fun solveA(text: String): Int {
     val (rulesText, messages) = text.replace("\r", "").split("\n\n")
-
-    val rules: List<Rule> = rulesText.lines()
-        .map { it.split(":") }
-        .sortedBy { it[0].toInt() }
-        .mapIndexed { i, (index, rulePart) ->
-            assert(index.toInt() == i)
-            buildRule(rulePart.trim())
-        }
-
-    return messages.split("\n").count { rules[0].matches(rules, it) }
+    val rules: List<Rule> = buildRules(rulesText)
+    val rule0 = rules[0].toRegex(rules).toRegex()
+    return messages.split("\n").count { rule0.matches(it) }
 }
+
+fun solveB(text: String): Int {
+    val (rulesText, messages) = text.replace("\r", "").split("\n\n")
+    val rules = buildRules(rulesText)
+
+    //New rules 8/11 gives rule 0 the following meaning:
+    //Any number (at least two) of 42 occurrences followed by a lower number (at least one) of 31 occurrences
+    return messages.split("\n").count { Rule.Rule0B.matches(rules, it) }
+}
+
+private fun buildRules(rulesText: String) = rulesText.lines()
+    .map { it.split(":") }
+    .sortedBy { it[0].toInt() }
+    .mapIndexed { i, (index, rulePart) ->
+        assert(index.toInt() == i)
+        buildRule(rulePart.trim())
+    }
 
 private fun buildRule(rulePart: String): Rule {
     return when {
@@ -82,27 +76,7 @@ private fun buildRule(rulePart: String): Rule {
             val (part1, part2) = rulePart.split("|")
             Rule.OrRule(part1.trim().split(" ").map { it.toInt() }, part2.trim().split(" ").map { it.toInt() })
         }
-        "\"" in rulePart -> Rule.LetterRule(rulePart.trim(' ', '"'))
+        '"' in rulePart -> Rule.LetterRule(rulePart.trim(' ', '"'))
         else -> Rule.SequenceRule(rulePart.split(" ").map { it.toInt() })
     }
-}
-
-
-fun solveB(text: String): Int {
-    val (rulesText, messages) = text.replace("\r", "").split("\n\n")
-
-
-    val rules = rulesText.lines()
-        .map { it.split(":") }
-        .sortedBy { it[0].toInt() }
-        .mapIndexed { i, (index, rulePart) ->
-            assert(index.toInt() == i)
-            buildRule(rulePart.trim())
-        }.toMutableList()
-
-    //New rules 8/11 gives rule 0 the following meaning:
-    //Any number (at least two) of 42 occurrences followed by a lower number (at least one) of 31 occurrences
-    rules[0] = Rule.Rule0()
-
-    return messages.split("\n").count { rules[0].matches(rules, it) }
 }
