b00bb312474b9e3d6ea291d63d9993a512c9c9ab
Renette Ros <supergirl0894+git@gmail.com>
2020-12-22 07:32:40 +0200

    Solve Day 22

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -1,10 +1,78 @@
 package day22
 
-fun solveA(lines: List<String>): Int {
-    TODO("Not implemented")
+import java.util.*
+
+fun solveA(text: String): Int {
+    val (part1, part2) = text.replace("\r", "").split("\n\n")
+
+    val player1Cards: Queue<Int> = LinkedList(part1.trim().lines().drop(1).map { it.toInt() })
+    val player2Cards: Queue<Int> = LinkedList(part2.trim().lines().drop(1).map { it.toInt() })
+
+    while (player1Cards.isNotEmpty() && player2Cards.isNotEmpty()) {
+        val player1 = player1Cards.poll()
+        val player2 = player2Cards.poll()
+
+        if (player1 > player2) {
+            player1Cards.add(player1)
+            player1Cards.add(player2)
+        } else {
+            player2Cards.add(player2)
+            player2Cards.add(player1)
+        }
+    }
+
+    val winningCards = if (player1Cards.isNotEmpty()) player1Cards else player2Cards
+
+    return winningCards.mapIndexed { i, value -> (winningCards.size - i) * value }.sum()
 }
 
+fun solveB(text: String): Int {
+    val (part1, part2) = text.replace("\r", "").split("\n\n")
 
-fun solveB(lines: List<String>): Int {
-    TODO("Not implemented")
+    val player1Cards = LinkedList(part1.trim().lines().drop(1).map { it.toInt() })
+    val player2Cards = LinkedList(part2.trim().lines().drop(1).map { it.toInt() })
+
+    val winner = recursiveCombat(player1Cards, player2Cards)
+
+    val winningCards = if (winner == 1) player1Cards else player2Cards
+
+    return winningCards.mapIndexed { i, value -> (winningCards.size - i) * value }.sum()
+}
+
+private fun recursiveCombat(player1Cards: LinkedList<Int>, player2Cards: LinkedList<Int>): Int {
+
+    val seenConfigurations = mutableSetOf<Pair<String, String>>()
+
+    while (player1Cards.isNotEmpty() && player2Cards.isNotEmpty()) {
+        val roundConfiguration = player1Cards.joinToString(",") to player2Cards.joinToString(",")
+
+        if (roundConfiguration in seenConfigurations) {
+            return 1
+        }
+
+        seenConfigurations.add(roundConfiguration)
+
+        val player1 = player1Cards.poll()
+        val player2 = player2Cards.poll()
+
+        val winner = if (player1 <= player1Cards.size && player2 <= player2Cards.size) {
+            val player1sublist = LinkedList(player1Cards.subList(0, player1))
+            val player2subList = LinkedList(player2Cards.subList(0, player2))
+            recursiveCombat(player1sublist, player2subList)
+        } else if (player1 > player2) {
+            1
+        } else {
+            2
+        }
+
+        if (winner == 1) {
+            player1Cards.add(player1)
+            player1Cards.add(player2)
+        } else {
+            player2Cards.add(player2)
+            player2Cards.add(player1)
+        }
+    }
+
+    return if (player1Cards.isNotEmpty()) 1 else 2
 }
--- src/test/kotlin/day22/Day22KtTest.kt
+++ src/test/kotlin/day22/Day22KtTest.kt
@@ -1,41 +1,31 @@
 package day22
 
 import helper.readDayFile
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-@Ignore
 internal class Day22KtTest {
 
     private val day = 22
 
     @Test
     fun sample1() {
-        val lines = readDayFile(day, "sample1.in").readLines()
+        val lines = readDayFile(day, "sample1.in").readText()
 
-        assertEquals(0, day25.solveA(lines))
-        assertEquals(0, day25.solveB(lines))
-    }
-
-    @Test
-    fun sample2() {
-        val lines = readDayFile(day, "sample2.in").readLines()
-
-        assertEquals(0, day25.solveA(lines))
-        assertEquals(0, day25.solveB(lines))
+        assertEquals(306, solveA(lines))
+        assertEquals(291, solveB(lines))
     }
 
     @Test
     fun solve() {
-        val lines = readDayFile(day, "input").readLines()
+        val lines = readDayFile(day, "input").readText()
 
-        val solveA = day25.solveA(lines)
+        val solveA = solveA(lines)
         println("A: $solveA")
-        assertEquals(0, solveA)
+        assertEquals(31308, solveA)
 
-        val solveB = day25.solveB(lines)
+        val solveB = solveB(lines)
         println("B: $solveB")
-        assertEquals(0, solveB)
+        assertEquals(33647, solveB)
     }
 }
